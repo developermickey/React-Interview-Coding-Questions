ee787ad138e80279e4f49593c91ff5746b64ed86
developermickey <mukesh.quazma@gmail.com>
2025-08-26 01:15:17 +0530

    10. Simple Form with Validation

---
+++ 10. Simple Form with Validation/src/App.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState } from "react";
+
+const App = () => {
+  const [form, setForm] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    if (!form.name || !form.email.includes("@")) {
+      setError("Please Enter a valid name and email");
+      return;
+    }
+
+    setError("");
+    console.log(form);
+  };
+  return (
+    <div>
+      <form onSubmit={submitForm}>
+        <input
+          type="text"
+          placeholder="Full Name"
+          onChange={(e) => setForm({ ...form, name: e.target.value })}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+        />
+        <button type="submit">Submit</button>
+
+        {error && <p style={{ color: "red" }}>{error}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default App;

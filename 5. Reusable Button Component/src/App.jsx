40a7c2af1b39ebc07fee1da492b240650f571aee
developermickey <mukesh.quazma@gmail.com>
2025-08-26 00:32:36 +0530

    5. Reusable Card  Component

---
+++ 5. Reusable Button Component/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./components/Card/Card";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+export default App;
---
+++ 5. Reusable Button Component/src/components/Card/Card.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "salmon",
+        color: "white",
+        padding: "20px",
+        borderRadius: "10px",
+      }}
+    >
+      <h1>This is Card heading</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit beatae
+        maiores blanditiis, rem cumque fuga aliquid. Harum repellendus cumque
+        odio.
+      </p>
+      <button>Learn More</button>
+    </div>
+  );
+};
+
+export default Card;
